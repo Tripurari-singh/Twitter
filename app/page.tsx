8e998bc2a6de6458a91e0790614057b3a123e728
Tripurari-singh <tripurari5821@gmail.com>
2026-05-06 16:56:20 +0530

    Dark mode

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 import { Button } from "@/components/ui/button";
+import ModeToggle from "@/components/ui/ToggleMode";
 import { Show, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
-      <Show when="signed-out">
+          <Show when="signed-out">
             <SignInButton />
               <SignUpButton>
                    <Button variant={"outline"} >
@@ -13,12 +14,7 @@ export default function Home() {
                    </Button>
               </SignUpButton>
       </Show>
-
-
-      <Show when="signed-in">
-              {/* <UserButton /> */}
-              
-      </Show>
-    </div>
+      <ModeToggle />
+          </div>
   );
 }
