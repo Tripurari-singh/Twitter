2acd195c383f60c2bd53b0cc15c49bec683672a9
Tripurari-singh <tripurari5821@gmail.com>
2026-05-06 13:28:36 +0530

    clerk Auth added

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,20 @@
+import { Show, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
-      Hello
+      <Show when="signed-out">
+            <SignInButton />
+              <SignUpButton>
+                <button className="bg-red-400 rounded-2xl m-4 ">
+                  Sign Up
+                </button>
+              </SignUpButton>
+            </Show>
+      <Show when="signed-in">
+              <UserButton />
+      </Show>
     </div>
   );
 }
