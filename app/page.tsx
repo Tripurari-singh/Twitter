b6a61c92481d0d91db86065df51604b21352302a
Tripurari-singh <tripurari5821@gmail.com>
2026-05-14 12:46:14 +0530

    prisma client generated

--- app/page.tsx
+++ app/page.tsx
@@ -2,16 +2,16 @@ import DemoOneFooter from "@/components/ui/Footer";
 import { HomeSections } from "@/components/ui/HomeSection";
 import { HeroGeometric } from "@/components/ui/HomeUI";
 import { Navbar1 } from "@/components/ui/navbar-1";
+import { prisma } from "@/lib/prisma";
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 
 export default async function Home() {
   const { userId } = await auth();
-
   if (userId) {
     redirect('/dashboard')
   }
-
+  
   return (
     <div>
       <Navbar1/>
