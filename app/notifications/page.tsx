fcbdec0ede23b795f1c16b46d268e53ed0b9391b
Tripurari-singh <tripurari5821@gmail.com>
2026-05-27 03:56:45 +0530

    refactor: remove old notifications pages (moved to protected group)

--- app/notifications/page.tsx
+++
@@ -1,9 +0,0 @@
-import NotificationList from "@/components/ui/NotificationList";
-
-export default function NotificationsPage() {
-  return (
-    <div className="max-w-2xl mx-auto py-6 px-4">
-      <NotificationList />
-    </div>
-  );
-}
